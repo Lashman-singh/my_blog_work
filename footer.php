5c4437092ea3a50bd1e183162f7ede5777deb747
Lashman Singh <lashmansingh38034@gmail.com>
2024-03-14 00:43:21 -0500

    uploading assignment on gitHub

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- 
+    Name: Lashman Singh
+    Date: 11-02-2024
+    Description: 
+-->
+<footer class ="footer">
+    <small class ="d-block text-end container">CopyMe &copy; <?=date('Y')?> * Built by Lashman Singh!</small>
+</footer>
---
+++ nav.php
@@ -0,0 +1,12 @@
+
+<!--     
+    Name: Lashman Singh
+    Date: 11- 02- 2024
+    Description:
+--> 
+<nav class="navbar">
+    <ul class="container">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="post.php" class="button-primary-ouline">+ New Blog</a></li>
+    </ul>
+</nav>
